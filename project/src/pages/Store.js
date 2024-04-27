21c25731683833da47b7c2b6c0201e3ef6d126b6
0empty <117061586+zeroempty2@users.noreply.github.com>
2024-04-27 20:45:02 +0900

    #15 <fix> Store컴포넌트 불필요한 구문 수정 및 삭제

--- project/src/pages/Store.js
+++ project/src/pages/Store.js
@@ -1,74 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import {Routes,Route,useParams,Link} from "react-router-dom";
-import axios from 'axios';
-import { URL_VARIABLE } from "./ExportUrl"; 
-import Review from "./Review";
+  import React, { useState, useEffect } from 'react';
+  import {useParams,Link} from "react-router-dom";
+  import axios from 'axios';
+  import { URL_VARIABLE } from "./ExportUrl"; 
 
-const Reviews = ({ reviewData }) => {
 
-  return (
-      <tr>
-          <td><Link to={`/review/${reviewData.id}`}>{reviewData.id}</Link></td>
-          <td>{reviewData.nickName}</td>
-          <td>{reviewData.createdAt}</td>
-          <td>{reviewData.likeCount}</td>
-      </tr>
-  );
-}
-
-const Store = () =>{
-    const { id } = useParams(); 
-    const [storeContents,setStoreContents] = useState();
-    const [reviews, setReviews] = useState([]);
-
-    useEffect(() => {
-    const fetchReviews = async () => {
-        try {
-            const response = await axios.get(URL_VARIABLE + "reviews/store/" + id);
-            console.log(response);
-            setReviews(response.data);
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    fetchReviews();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-}, []); 
-
-    useEffect(() => {
-        const fetchStore = async () => {
-            try {
-                const response = await axios.get(URL_VARIABLE + "stores/" + id);
-                setStoreContents(response.data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-        fetchStore();
-    },[id])
+  const Reviews = ({ reviewData }) => {
 
     return (
-        <div>
-          {storeContents && (
-            <div>
-              <p>가게 이름: {storeContents.storeName}</p>
-              <p>평점: {storeContents.starRate}</p>
-              <p>가게 소개: {storeContents.aboutStore}</p>
-            </div>
-          )}
+        <tr>
+            <td><Link to={`/review/${reviewData.id}`}>{reviewData.id}</Link></td>
+            <td>{reviewData.nickName}</td>
+            <td>{reviewData.createdAt}</td>
+            <td>{reviewData.likeCount}</td>
+        </tr>
+    );
+  }
+
+  const Store = () =>{
+      const { id } = useParams(); 
+      const [storeContents,setStoreContents] = useState();
+      const [reviews, setReviews] = useState([]);
+
+      useEffect(() => {
+      const fetchReviews = async () => {
+          try {
+              const response = await axios.get(URL_VARIABLE + "reviews/store/" + id);
+              console.log(response);
+              setReviews(response.data);
+          } catch (error) {
+              console.error(error);
+          }
+      };
+
+      fetchReviews();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); 
+
+      useEffect(() => {
+          const fetchStore = async () => {
+              try {
+                  const response = await axios.get(URL_VARIABLE + "stores/" + id);
+                  setStoreContents(response.data);
+              } catch (error) {
+                  console.error(error);
+              }
+          };
+          fetchStore();
+      },[id])
+
+      return (
           <div>
-            <p>리뷰</p>
+            {storeContents && (
+              <div>
+                <p>가게 이름: {storeContents.storeName}</p>
+                <p>평점: {storeContents.starRate}</p>
+                <p>가게 소개: {storeContents.aboutStore}</p>
+              </div>
+            )}
+            <div>
+              <p>리뷰</p>
 
-          {reviews.length > 0 ? reviews.map(review => <Reviews key={review.id} reviewData={review} />) : (<p>리뷰가 없습니다</p>)}
+            {reviews.length > 0 ? reviews.map(review => <Reviews key={review.id} reviewData={review} />) : (<p>리뷰가 없습니다</p>)}
+            <Link to = {`/reservations/${id}`}><button>예약</button></Link > 
+            </div>
+
           </div>
-        <Routes>
-          <Route path="/review/:id" element={<Review />} />
-        </Routes>
-        </div>
-        
-      );
+          
+        );
 
-}
+  }
 
-export default Store;
+  export default Store;
