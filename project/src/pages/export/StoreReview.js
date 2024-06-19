6816e33e8bec599529926974f922368b80b9b285
2zeroempty@gmail.com <2zeroempty@gmail.com>
2024-06-19 19:25:49 +0900

    #15 <fix> storeReview 로딩시 reviewPicture도 같이 로딩

--- project/src/pages/export/StoreReview.js
+++ project/src/pages/export/StoreReview.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 import '../css/StoreReview.css';
 
 const StoreReview = ({ reviewData }) => {
+    const [reviewPictures] = useState(reviewData.reviewPictures);
+    const reviewUrl = reviewPictures[0].reviewPictureUrl;
+
     return (
-        <div className="store-review">
+        <div>
+            <div className='store-review-image' style={{ backgroundImage: `url(${reviewUrl})` }}></div>
+             <div className="store-review">
             <p><Link to={`/reviews/${reviewData.reviewId}`}> 제목 : {reviewData.reviewTitle} </Link></p>
             <p>평점 : {reviewData.totalRating}</p>
             <p>작성일 : {reviewData.createdAt}</p>
             <p>좋아요 : {reviewData.likeCount}</p>
         </div>
+        <div className='button-div'></div>
+        </div>
+       
     );
 };
+
 export default StoreReview;
